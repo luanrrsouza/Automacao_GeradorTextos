d79df02a4e73ef223fa70cb513c475abc6119365
luanrrsouza <luanrrdsouza@gmail.com>
2024-10-27 00:52:01 -0300

    Criando o for para inteirar pelos 75 temas

---
+++ array_temas.py
@@ -0,0 +1,26 @@
+temas = [
+    "Celula procariotica e eucariotica", "Divisao celular: mitose e meiose", "Teoria celular", "Organizacao dos seres vivos", 
+    "Membrana plasmatica e transporte celular", "Ciclo do carbono", "Fotossintese", "Respiracao celular", 
+    "Ciclo do nitrogenio", "Ciclo do oxigenio", "Cadeias e teias alimentares", "Ecossistemas e biomas", 
+    "Biogeografia", "Selecao natural", "Mutacao genetica", "Fluxo genico e deriva genetica", "Especiacao", 
+    "Sistematica e classificacao dos seres vivos", "Teoria evolutiva de Darwin e Lamarck", "Genetica mendeliana", 
+    "Heranca ligada ao sexo", "Teoria cromossomica da heranca", "Mutacao e recombinacao genetica", 
+    "Clonagem e engenharia genetica", "Acidos nucleicos: DNA e RNA", "Sintese de proteinas", "Mutacao genetica", 
+    "Metabolismo celular", "Enzimas e suas funcoes", "Hormonos vegetais", "Sistemas de defesa: imunidade inata e adquirida", 
+    "Ecologia de populacoes", "Biodiversidade e conservacao", "Sucessao ecologica", "Fatores abioticos e bioticos", 
+    "Poluicao e suas consequencias", "Sustentabilidade ambiental", "Relacoes ecologicas: simbiose, mutualismo, parasitismo", 
+    "Migracoes de animais", "Adaptabilidade dos organismos", "Embriologia", "Fisiologia humana: sistema nervoso", 
+    "Fisiologia humana: sistema digestorio", "Fisiologia humana: sistema circulatorio", "Fisiologia humana: sistema respiratorio", 
+    "Fisiologia humana: sistema excretor", "Fisiologia humana: sistema reprodutor", "Neurotransmissores e o cerebro", 
+    "Homeostase", "Endocrinologia: glandulas e hormonos", "Biomecanica dos musculos e esqueleto", 
+    "Sistema imunitario e vacinas", "Celulas-tronco", "Biotecnologia e bioetica", "Biorremediacao", 
+    "Impactos das mudancas climaticas nos ecossistemas", "Zoologia: invertebrados", "Zoologia: vertebrados", 
+    "Botanica: anatomia vegetal", "Botanica: fisiologia vegetal", "Microbiologia: virus", "Microbiologia: bacterias", 
+    "Microbiologia: protozoarios", "Microbiologia: fungos", "Parasitologia: protozooses e helmintoses", 
+    "Doencas geneticas e hereditarias", "Biosseguranca em laboratorios", "Tecnicas de biologia molecular", 
+    "Carboidratos, proteinas e lipidios", "Ecotoxicologia", "Estudo da bioluminescencia", 
+    "Principios da termodinamica em biologia", "Taxonomia e filogenia", "Comportamento animal: instinto e aprendizado", 
+    "Principios de bioquimica"
+]
+
+
---
+++ auxiliar.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+
+
+time.sleep(5)
+print(pyautogui.position())
+pyautogui.position()
+
+#pyautogui.scroll(1000)
