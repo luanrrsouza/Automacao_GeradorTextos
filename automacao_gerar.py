ba44ab38490bd01d47615ffad5a96e9f0bcfdba2
Luan Rodrigo <136761126+luanrrsouza@users.noreply.github.com>
2024-11-03 14:23:09 -0300

    Projeto final
    finalizando o projeto, com o tratamento do cloudflare

--- automacao_gerar.py
+++ automacao_gerar.py
@@ -3,44 +3,50 @@ import time
 from array_temas import temas
 
 pyautogui.PAUSE = 1
-pyautogui.press("win")
-pyautogui.write("Google Chrome")
-pyautogui.press("enter")
-pyautogui.write("https://www.editpad.org/tool/br/gerador-de-redacao")
-pyautogui.press("enter")
-time.sleep(5)
+pyautogui.FAILSAFE = True  
 
-for tema in temas:
-    pyautogui.click(x=703, y=353)  
-    time.sleep(1)
-    pyautogui.write(f'Gere um texto sobre {tema} e coloque 10 perguntas e respostas elaboradas')
+def automacao():
     
-    pyautogui.click(x=601, y=463)  
-    pyautogui.click(x=974, y=537)  
-    time.sleep(20) 
-    # todo: CRIAR UMA EXCEÇÃO PARA QUANDO PEDIR PARA CLICAR EM "VERIFICAR SE VOCê É HUMANO"
-    pyautogui.click(x=1344, y=148)  
-    time.sleep(4)
-
-    pyautogui.click(x=1774, y=110)  
-    pyautogui.hotkey("ctrl", "c")  
-    
-    pyautogui.click(x=966, y=62)  
-    pyautogui.write(r"C:\Temp\ws- VSCode\Python\OneBitCode")  
+    pyautogui.press("win")
+    pyautogui.write("Edge")
     pyautogui.press("enter")
-    
-    pyautogui.click(x=1056, y=634)  
-    pyautogui.hotkey("ctrl", "v")  
+    pyautogui.write("https://www.editpad.org/tool/br/gerador-de-redacao")
+    pyautogui.press("enter")
+    time.sleep(5)
 
-    pyautogui.hotkey("ctrl", "w")  
-    pyautogui.scroll(1000)  
-    pyautogui.click(x=810, y=349)
-    pyautogui.hotkey("ctrl", "a")
-    pyautogui.press("backspace")
+    for tema in temas:
+        
+        pyautogui.click(x=647, y=307)  
+        time.sleep(1)
+        pyautogui.write(f'Gere um texto sobre {tema} e coloque 10 perguntas e respostas elaboradas')
+        
+        pyautogui.click(x=647, y=307)  
+        pyautogui.click(x=952, y=475)  
+        time.sleep(3)
+        pyautogui.click(x=852, y=366)
+        
+        time.sleep(20) 
+        
+        pyautogui.click(x=1303, y=124)  
+        time.sleep(4)
+
+        pyautogui.click(x=1741, y=129)  
+        pyautogui.hotkey("ctrl", "c")  
+        
+        pyautogui.click(x=699, y=54)  
+        pyautogui.write(r"C:\Users\L4R1_\Music\passei direto outubro")  
+        pyautogui.press("enter")
+        
+        pyautogui.click(x=984, y=443)  
+        pyautogui.hotkey("ctrl", "v")  
+
+        pyautogui.hotkey("ctrl", "w")  
+        pyautogui.scroll(1000)  
+        pyautogui.click(x=669, y=303)
+        pyautogui.hotkey("ctrl", "a")
+        pyautogui.press("backspace")
+
+    print("Processo concluído!")
 
 
-print("Processo concluído!")
-
-
-
-
+automacao()
